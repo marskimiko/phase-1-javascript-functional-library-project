4ae5bfbcb1a6e281dbcaf4da60925198d643bc8f
marisasaunders <marisaksaunders@gmail.com>
2022-02-24 17:20:17 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+const myEach = function(collection, callback) {
+
+// iterates over the collection of elements, pass each element in turn to the callback function. returns the original, unmodified, collection   
+
+return collection
+
+}
